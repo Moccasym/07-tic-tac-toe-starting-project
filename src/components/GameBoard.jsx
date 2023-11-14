a63948456453d2632358c8e87d4fed6600fbb657
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-11-14 20:30:29 +0100

    Game Board working with one Symbol

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -11,19 +11,20 @@ const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
 const handleSelectSquare = (rowIndex, colIndex) => {
     setGameBoard((prevGameBoard) => {
-        prevGameBoard[rowIndex][colIndex] = 'X';
-        return prevGameBoard;
+        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+        updatedBoard[rowIndex][colIndex] = 'X';
+        return updatedBoard;
     });
 }
 
     return(
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => 
+            {gameBoard.map((row, rowIndex) => 
             <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => 
                     <li key={colIndex}>
-                        <button onClick={handleSelectSquare}>{playerSymbol}</button>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)}
