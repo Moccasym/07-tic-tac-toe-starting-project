f6db4eb97913408fe511bc6d482bced5fbe1b37a
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-11-15 20:23:47 +0100

    Log component working

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 
-const Log = () => {
+const Log = ({ turns }) => {
     return(
         <ol id="log">
-
-        </ol>
+            {turns.map(turn => 
+            <li key={`${turn.square.row}${turn.square.col}`}>
+                {turn.player} selected {turn.square.row}, {turn.square.col}
+            </li>)}
+        </ol> 
 
     )
 }
