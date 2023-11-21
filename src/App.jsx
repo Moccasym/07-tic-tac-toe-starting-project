114135d61f33f84f75a99179f1200bda90eeb22b
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-11-21 21:38:08 +0100

    added comments and cleaned up a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Importing necessary components and constants
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log";
@@ -7,107 +8,126 @@ import { WINNING_COMBINATIONS } from "./components/winning-combinations";
 
 import React, { useState } from "react"
 
-
+// Constant representing player names
 const PLAYERS = {
   X: 'Player 1',
   O: 'Player 2'
 };
 
-// initial empty game Board
+// Initial empty game board
 const initialGameBoard = [
-  [null,null,null],
-  [null,null,null],
-  [null,null,null],
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ]
 
-//helper function outside of component function
+// Helper function outside of component function to determine the active player
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
-      
+
+  // If there are turns and the first player is 'X', set the current player to 'O'
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
     currentPlayer = 'O';
   }
-  return (currentPlayer);
+  return currentPlayer;
 }
 
-
-
+// Function to derive the winner based on the current game board and players
 const deriveWinner = (gameBoard, players) => {
   let winner;
 
-  for (const combination of WINNING_COMBINATIONS){
+  // Check each winning combination
+  for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
-    if (firstSquareSymbol && 
-      firstSquareSymbol === secondSquareSymbol && 
-      firstSquareSymbol === thirdSquareSymbol){
+    // If all three symbols in a combination are the same, set the winner
+    if (firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol) {
       winner = players[firstSquareSymbol];
-  }}
-return winner;
+    }
+  }
+  return winner;
 }
 
+// Function to derive the game board based on the list of turns
 const deriveGameBoard = (gameTurns) => {
   let gameBoard = [...initialGameBoard.map(array => [...array])];
 
+  // Update the game board based on the turns
   for (const turn of gameTurns) {
-      const { square, player} = turn;
-      const { row, col } = square;
+    const { square, player } = turn;
+    const { row, col } = square;
 
-      gameBoard[row][col] = player;
+    gameBoard[row][col] = player;
   }
   return gameBoard;
 }
 
 function App() {
-  const [players, setPlayers] = useState({PLAYERS});
+  // State for players and game turns
+  const [players, setPlayers] = useState({ PLAYERS });
   const [gameTurns, setGameTurns] = useState([]);
 
+  // Derive active player, game board, winner, and draw status
   const activePlayer = deriveActivePlayer(gameTurns);
   const gameBoard = deriveGameBoard(gameTurns);
   const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
+  // Handle selecting a square on the game board
   const handleSelectSquare = (rowIndex, colIndex) => {
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
-      
-      const updatedTurns = [{ 
-        square: {row : rowIndex, col: colIndex}, 
-        player: activePlayer }, 
-        ...prevTurns];
-        return updatedTurns;
+
+      // Add the new turn to the list of turns
+      const updatedTurns = [{
+        square: { row: rowIndex, col: colIndex },
+        player: activePlayer
+      },
+      ...prevTurns];
+      return updatedTurns;
     });
   }
 
-    const handleRestart = () => {
-      setGameTurns([]);
-    }
+  // Handle restarting the game
+  const handleRestart = () => {
+    setGameTurns([]);
+  }
 
-    const handlePlayerNameChange = (symbol, newName) => {
-      setPlayers(prevPlayers => {
-        return{
-          ...prevPlayers,
-          [symbol] : newName
-        };
-      });
-    }
+  // Handle changing the name of a player
+  const handlePlayerNameChange = (symbol, newName) => {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
+  }
+
   return (
+    // Main component structure
     <main>
       <div id="game-container">
+        {/* Display the players */}
         <ol id="players" className="highlight-player">
-          <Player isActive={activePlayer === 'X'} initialName={PLAYERS.X} symbol="X" onChangeName={handlePlayerNameChange}/>
-          <Player isActive={activePlayer === 'O'} initialName={PLAYERS.O} symbol="O" onChangeName={handlePlayerNameChange}/>
+          <Player isActive={activePlayer === 'X'} initialName={PLAYERS.X} symbol="X" onChangeName={handlePlayerNameChange} />
+          <Player isActive={activePlayer === 'O'} initialName={PLAYERS.O} symbol="O" onChangeName={handlePlayerNameChange} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
-        <GameBoard 
-          onSelectSquare={handleSelectSquare} 
-          board={gameBoard}/>
+        {/* Display the game over component if there is a winner or a draw */}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
+        {/* Display the game board */}
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          board={gameBoard} />
       </div>
-      <Log turns={gameTurns}/>
+      {/* Display the game log */}
+      <Log turns={gameTurns} />
     </main>
   )
 }
 
+// Export the App component
 export default App;
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,20 @@
+// Importing React library and the useState hook
 import React, { useState } from "react";
 
-const Player = ({initialName, symbol, isActive, onChangeName}) => {
-    const [isEditing, setIsEditing] = useState(false)
+// Functional component representing a player in the game
+const Player = ({ initialName, symbol, isActive, onChangeName }) => {
+    // State to manage whether the player name is being edited
+    const [isEditing, setIsEditing] = useState(false);
+
+    // State to store the current player name
     const [playerName, setPlayerName] = useState(initialName);
 
-  
-
+    // Event handler to toggle editing mode and save changes
     const onEditHandler = () => {
         setIsEditing((editing) => !editing);
-        if (isEditing){
+        
+        // If editing is true, save changes using the onChangeName prop
+        if (isEditing) {
             onChangeName(symbol, playerName);
         }
     }
@@ -19,25 +25,28 @@ const Player = ({initialName, symbol, isActive, onChangeName}) => {
         setPlayerName(event.target.value);
     };
 
+    // Variable to conditionally render player name as text or input field
     let editPlayerName = <span className="player-name">{playerName}</span>
 
+    // If in editing mode, render an input field for editing the player name
     if (isEditing) {
         editPlayerName = (<input type="text" required value={playerName} onChange={handleInputChange} />)
-    } 
+    }
 
-    return(
+    // Rendering the player component
+    return (
         <li className={isActive ? 'active' : undefined}>
             <span className='player'>
-              {editPlayerName}
-              <span className="player-symbol">{symbol}</span>
+                {/* Displaying the player name (text or input field) */}
+                {editPlayerName}
+                {/* Displaying the player symbol */}
+                <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={onEditHandler}>{isEditing ? 'Save':'Edit'}</button>
-          </li>
+            {/* Button to toggle between editing and saving mode */}
+            <button onClick={onEditHandler}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
     )
 }
 
+// Exporting the Player component
 export default Player;
-
-
-
-
